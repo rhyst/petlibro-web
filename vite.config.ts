e8693b5c095e97ac4eedabd90c4b3ffbb9ff40aa
Rhys Tyers <5313660+rhyst@users.noreply.github.com>
2026-05-11 21:33:34 +0100

    Bump all deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: process.env.VITE_BASENAME || "/",
   resolve: {
     alias: {
